c7e999d255e01cd4fcb38340ee7869cc64a65917
neominal.solutions <neominal.solutions@gmail.com>
2025-01-08 14:04:08 +0300

    Add project files.

---
+++ src/Order.Saga/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMassTransit(cfg =>
+{
+
+  cfg.UsingRabbitMq((context, config) =>
+  {
+    config.Host(builder.Configuration.GetConnectionString("RabbitConn"));
+    config.ReceiveEndpoint("order-saga-queue", configurator =>
+    {
+      
+    });
+  });
+});
+
+
+
+
+
