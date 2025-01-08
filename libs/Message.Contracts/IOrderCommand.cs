8b18679ab587adce690383db09981a87c75736af
neominal.solutions <neominal.solutions@gmail.com>
2025-01-08 14:13:31 +0300

    Message Contracts

---
+++ libs/Message.Contracts/IOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message.Contracts
+{
+  // Not: OrderCommand Saga sürecini ilk başlatan bir komut olması sebebi ile Correlationıd burada tutmadık. Bundan sonraki süreçlerde operasyonların tek bir ıd üzerinden işlenmesi ve takibi amaçlı tanımlanacaktır.
+
+  public interface IOrderCommand
+  {
+    int OrderId { get; set; }
+    string OrderCode { get; set; }
+  }
+}
---
+++ libs/Message.Contracts/IOrderProceeded.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message.Contracts
+{
+  public interface IOrderProceeded
+  {
+    Guid CorrelationId { get; set; } // Saga State Machine da süreç takibi için inital olarak başlatıp tüm state süreci bu ıd üzerinden devam edecek ki operasyonlar birbirinde izole olsun ve takip edilsin
+    int OrderId { get; set; }
+
+  }
+}
